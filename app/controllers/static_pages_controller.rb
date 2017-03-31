74961b9e8bd2bf92852a204e9095ca54a63ae209
ColinW520 <colinhagan@gmail.com>
2017-03-31 12:44:15 -0500

    progress commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  layout 'static_views'
   skip_before_action :authenticate_user!
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   match '/home' => "static_pages#home", via: [:get]
   match '/contact' => "static_pages#contact", via: [:get]
-  match '/pricing' => "static_pages#pricing", via: [:get]
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     invitations: 'users/invitations'
   }
+
+  resource :dashboard, controller: 'dashboard'
 
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :employees

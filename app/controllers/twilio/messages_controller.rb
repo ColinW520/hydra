dfd96ce7a8333050271663f289556ed57a15d0fc
ColinW520 <colinhagan@gmail.com>
2017-06-30 15:28:21 -0500

    log the prams

--- app/controllers/twilio/messages_controller.rb
+++ app/controllers/twilio/messages_controller.rb
@@ -1,5 +1,7 @@
 class Twilio::MessagesController < Twilio::BaseController
   def create
+    Rails.logger.info params
+
     Twilio::Messages::ReceivingWorker.new.perform(params) if Rails.env.development?
     Twilio::Messages::ReceivingWorker.perform_async(params) if Rails.env.production?
   end

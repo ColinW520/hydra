13a30fc9289e1059b36d087fd5efab6bba43e33c
ColinW520 <colinhagan@gmail.com>
2017-07-01 14:28:44 -0500

    what are we actually passing here in prod...

--- app/controllers/twilio/messages_controller.rb
+++ app/controllers/twilio/messages_controller.rb
@@ -1,12 +1,12 @@
 class Twilio::MessagesController < Twilio::BaseController
   def create
-    Twilio::Messages::ReceivingWorker.new.perform(twilio_message_params) if Rails.env.development?
-    Twilio::Messages::ReceivingWorker.perform_async(twilio_message_params) if Rails.env.production?
+    Twilio::Messages::ReceivingWorker.new.perform(twilio_message_params.to_h) if Rails.env.development?
+    Twilio::Messages::ReceivingWorker.perform_async(twilio_message_params.to_h) if Rails.env.production?
   end
 
 private
 
   def twilio_message_params
-    params.permit(["ToCountry", "ToState", "SmsMessageSid", "NumMedia", "ToCity", "FromZip", "SmsSid", "FromState", "SmsStatus", "FromCity", "Body", "FromCountry", "To", "ToZip", "NumSegments", "MessageSid", "AccountSid", "From", "ApiVersion"]).to_h
+    params.permit([:ToCountry, :ToState, :SmsMessageSid, :NumMedia, :ToCity, :FromZip, :SmsSid, :FromState, :SmsStatus, :FromCity, :Body, :FromCountry, :To, :ToZip, :NumSegments, :MessageSid, :AccountSid, :From, :ApiVersion])
   end
 end

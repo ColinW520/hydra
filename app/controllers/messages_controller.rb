f7e13f1eefa2213d6cec3459a072f07c3c62e958
ColinW520 <colinhagan@gmail.com>
2017-07-11 00:02:18 -0500

    getting the sort right

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
     messages_scope = current_user.organization.messages.filter_by(params)
 
     sort_columns = [
-      [:date, 'messages.sent_at'],
+      [:date, 'messages.created_at'],
       [:direction, 'messages.direction'],
       [:name, 'contacts.first_name'],
       [:to, 'messages.to'],
@@ -12,8 +12,8 @@ class MessagesController < ApplicationController
     ]
 
     respond_to do |format|
-      format.html { smart_listing_create :messages, messages_scope, partial: 'messages/listing', sort_attributes: sort_columns, default_sort: { created_at: :desc }, page_sizes: [25, 50, 100, 150, 200] }
-      format.js { smart_listing_create :messages, messages_scope, partial: 'messages/listing', sort_attributes: sort_columns, default_sort: { created_at: :desc }, page_sizes: [25, 50, 100, 150, 200] }
+      format.html { smart_listing_create :messages, messages_scope, partial: 'messages/listing', sort_attributes: sort_columns, default_sort: { date: 'DESC' }, page_sizes: [25, 50, 100, 150, 200] }
+      format.js { smart_listing_create :messages, messages_scope, partial: 'messages/listing', sort_attributes: sort_columns, default_sort: { date: 'DESC' }, page_sizes: [25, 50, 100, 150, 200] }
       format.csv { send_data messages_scope.to_csv, filename: "messages_as_of-#{Time.now}.csv" }
     end
   end

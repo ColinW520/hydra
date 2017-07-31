0d43bd8070f04758c6f2aa1c750b84bf95169fae
ColinW520 <colinhagan@gmail.com>
2017-07-31 17:15:43 -0500

    move this to a consistent location

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,14 @@ module Hydra
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'aptexx-hydra.herokuapp.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end

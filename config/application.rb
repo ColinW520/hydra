f4587b66abbcd90e602026bea154d36b90444f4a
ColinW520 <colinhagan@gmail.com>
2017-07-31 22:17:56 -0500

    only prod should serve up in SSL

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ module Hydra
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.force_ssl = true
-
     config.autoload_paths << Rails.root.join('lib', 'inputs')
 
     # ActsAsTaggableOn.force_binary_collation = true

9a58c596eae1513f5cb642b6a5410b12ac273417
ColinW520 <colinhagan@gmail.com>
2017-08-08 16:27:03 -0700

    crash resolution

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -3,7 +3,7 @@ class MediaItem < ApplicationRecord
   delegate :created_by, to: :message_request
 
   has_attached_file :image,
-                    hash_secret: 'a92342be23ad9827634654a5617646'
+                    hash_secret: 'a92342be23ad9827634654a5617646',
                     path: 'message_request_media_items/:organization_id/:created_year/:created_month/image.:extension'
 
 end

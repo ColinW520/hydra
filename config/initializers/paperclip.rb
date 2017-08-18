aa829ffb7fa43ef0d59e67eb0372840d71656455
ColinW520 <colinhagan@gmail.com>
2017-08-18 16:23:53 -0500

    trust our users for now I guess

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,10 @@
 # Paperclip.options[:content_type_mappings] = { csv: 'application/vnd.ms-excel' }
+
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end

da48390b602e5d61a502ed9385cba864eb290eab
ColinW520 <colinhagan@gmail.com>
2017-08-18 16:10:44 -0500

    capture import failure messages #40

--- app/workers/imports/import_starting_worker.rb
+++ app/workers/imports/import_starting_worker.rb
@@ -12,19 +12,21 @@ class Imports::ImportStartingWorker
       remove_zero_values: false, # opposite of the default here, we do want values even if they have a numeric value == 0 (zero)
       convert_values_to_numeric: false # converts strings containing Integers or Floats to the appropriate class, also accepts either {:except => [:key1,:key2]} or {:only => :key3}
     }
-    the_import = Import.find import_id
-    return if the_import.is_enqueued?
-    the_import.update_attribute(:is_enqueued, true)
+    @the_import = Import.find import_id
+    return if @the_import.is_enqueued?
+    @the_import.update_attribute(:is_enqueued, true)
 
-    file = open(the_import.datafile.url)
+    file = open(@the_import.datafile.url)
 
     SmarterCSV.process(file, default_options).each do |chunk|
       chunk.each do |row|
-        Imports::ImportContactRowWorker.new.perform(row, the_import.id) if Rails.env.development?
-        Imports::ImportContactRowWorker.perform_async(row, the_import.id) if Rails.env.production?
+        Imports::ImportContactRowWorker.new.perform(row, @the_import.id) if Rails.env.development?
+        Imports::ImportContactRowWorker.perform_async(row, @the_import.id) if Rails.env.production?
       end
     end
 
-    the_import.update_attribute(:is_enqueued, false)
+    @the_import.update_attribute(:is_enqueued, false)
+  rescue => error
+    @the_import.update_attributes(status: 'failed', message: error.message, is_enqueued: false)
   end
 end

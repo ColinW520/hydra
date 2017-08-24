3bee4af64cc1fa0d1a74113d761e413485dd5d34
ColinW520 <colinhagan@gmail.com>
2017-08-24 11:59:29 -0500

    bugfix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -43,7 +43,7 @@ class Contact < ApplicationRecord
   end
 
   def self.filter_by(params)
-    params = params.to_unsafe_hash
+    params = params.with_indifferent_access
     contacts_scope = self.includes(:organization)
     contacts_scope = contacts_scope.not_removed
     contacts_scope = contacts_scope.where(internal_identifier: params[:internal_identifier]) if params[:internal_identifier].present?

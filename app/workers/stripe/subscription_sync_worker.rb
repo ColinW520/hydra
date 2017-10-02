daf044f9b20a400f0622f2707348e40f46139dd4
ColinW520 <colinhagan@gmail.com>
2017-10-01 23:59:09 -0500

    bugix

--- app/workers/stripe/subscription_sync_worker.rb
+++ app/workers/stripe/subscription_sync_worker.rb
@@ -5,7 +5,7 @@ class Stripe::SubscriptionSyncWorker
     Subscription.find_each do |local_subscription|
       stripe_subscription = Stripe::Subscription.retrieve local_subscription.stripe_id
       local_subscription.canceled_at = stripe_subscription.canceled_at if stripe_subscription.canceled_at.present?
-      local_subscription.status = stripe_subscription.status
+      local_subscription.current_status = stripe_subscription.status
       local_subscription.save!
     end
   end

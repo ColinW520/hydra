fb0a849a03803e4f86050f5492054f2ee971f818
ColinW520 <colinhagan@gmail.com>
2017-10-10 15:42:33 -0500

    hide this form when complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # MAILER
   config.action_mailer.default_url_options = { host: 'hydra.dev' }
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 

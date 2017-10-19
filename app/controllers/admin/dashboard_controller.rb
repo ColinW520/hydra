480353b7a6397cec0f484ad0b5ded55e63116040
ColinW520 <colinhagan@gmail.com>
2017-10-19 12:45:35 -0500

    support multiple series, easily

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,9 @@
 class Admin::DashboardController < Admin::BaseController
   def show
-    range_start = params[:range_start] ||= 1.month.ago
-    range_end = params[:range_start] ||= Time.now
-
     @activity_hash = [
-      { name: "Inbound Messages", data: Message.inbound.group_by_day(:created_at, range: range_start..range_end).count },
-      { name:"Outbound Messages", data: MessageRequest.sent.group_by_day(:processed_at, range: range_start..range_end).count },
-      { name: "Inbound Calls", data: CallLog.group_by_day(:created_at, range: range_start..range_end).count }
+      { name: "Inbound Messages", data: Message.inbound.group_by_day(:created_at, range: 1.month.ago.beginning_of_day..Time.now).count },
+      { name:"Outbound Messages", data: MessageRequest.sent.group_by_day(:processed_at, range: 1.month.ago.beginning_of_day..Time.now).count },
+      { name: "Inbound Calls", data: CallLog.group_by_day(:created_at, range: 1.month.ago.beginning_of_day..Time.now).count }
     ]
   end
 end

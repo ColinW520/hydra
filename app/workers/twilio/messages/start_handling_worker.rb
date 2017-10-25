5ac4c85fef61cba90e0d89543a50ec3bf322a400
ColinW520 <colinhagan@gmail.com>
2017-10-24 22:17:51 -0500

    bug resolution

--- app/workers/twilio/messages/start_handling_worker.rb
+++ app/workers/twilio/messages/start_handling_worker.rb
@@ -1,7 +1,7 @@
 class Twilio::Messages::StartHandlingWorker < Twilio::BaseWorker
   def perform(message_id)
     @message = Message.find message_id
-    @stop = Stop.where(line_id: @message.line_id, contact_id: @message.contact_id)
+    @stop = Stop.where(line_id: @message.line_id, contact_id: @message.contact_id).first
     @stop.update_attributes(rescinded_at: @message.created_at)
   end
 end
